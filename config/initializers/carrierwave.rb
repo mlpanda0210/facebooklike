1cfdd05ba63d8e2dafcbddc5a47eb431c0a2638b
tachiken0210 <kenji_tachibana@MBR14030.local>
2017-02-26 17:25:36 +0900

    carrierwave設定変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,11 +12,11 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'fb-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fb-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.fog_directory = 'fb-develop'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fb-develop'
     when 'test'
 config.fog_directory = 'テスト環境用のバケット名'
 config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
